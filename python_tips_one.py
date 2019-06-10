78920b07038ac49daf6c26a7d61456399232703c
obetron <erenbasaran@gmail.com>
2019-06-10 14:31:16 +0300

    python tip one added.

---
+++ python_tips_one.py
@@ -0,0 +1,15 @@
+#simple usage of if condition
+condition = True
+
+if condition:
+    x = 1
+else:
+    x = 0
+
+print(x)
+
+#this code written in one line like this
+
+x = 1 if condition else 0
+
+print(x)
