09074bf78974b4b9021ccd073d6a658ea32fc712
obetron <erenbasaran@gmail.com>
2019-06-11 20:56:21 +0300

    python tip six added.

--- python_tips_six.py
+++ python_tips_six.py
@@ -23,6 +23,7 @@ print(a)
 OUTPUT:
 1
 """
+
 #more variables than values
 # a, b, c = (1, 2)
 """
@@ -37,5 +38,59 @@ OUTPUT:
 ValueError: too many values to unpack
 """
 
-import sys
-print(sys.executable)
+#
+a, b, *c = (1, 2, 3, 4, 5)
+print(a)
+print(b)
+print(c)
+"""
+(with python3 and higher)
+OUTPUT:
+1
+2
+[3, 4, 5]
+"""
+
+d, e, *_ = (1, 2, 3, 4, 5)
+print(d)
+print(e)
+"""
+OUTPUT:
+1
+2
+"""
+
+f, g, _, *h = (1, 2, 3, 4, 5)
+print(f)
+print(g)
+print(h)
+"""
+OUTPUT:
+1
+2
+[4, 5]
+"""
+
+i, j, *_, k = (1, 2, 3, 4, 5)
+print(i)
+print(j)
+print(k)
+"""
+OUTPUT:
+1
+2
+5
+"""
+
+l, m, *n, o = (1, 2, 3, 4, 5)
+print(l)
+print(m)
+print(n)
+print(o)
+"""
+OUTPUT:
+1
+2
+[3, 4]
+5
+"""
